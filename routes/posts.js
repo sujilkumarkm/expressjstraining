8cd97a0057b76ee6812565c390a408280a55266c
sujilkumarkm <kmsujilkumar@gmail.com>
2025-11-09 23:53:11 +0000

    routes added

---
+++ routes/posts.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const router = express.Router();
+
+//routes    
+let posts = [
+    { id: 1, title: 'Best Post One'},
+    { id: 2, title: 'Silver Post Two'},
+    { id: 3, title: 'Post Three'},
+];
+
+// get all posts
+router.get('/', (req, res) => {
+    const limit = parseInt(req.query.limit);
+    if(!isNaN(limit) && limit > 0){ {
+        return res.status(200).json(posts.slice(0, limit));
+    } } 
+        res.status(200).json(posts);
+});
+
+//get single post
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    // res.status(200).json(posts.filter(post => post.id === id));
+    if(!post){
+       return res
+       .status(404)
+       .json({ msg: `A Post with ID ${id} is not found :( Ja ja ja...!`});
+    }
+       res.status(200).json(post);
+}); 
+
+export default router;
--- server.js
+++ server.js
@@ -1,42 +1,11 @@
-const express = require('express');
-const { cp } = require('fs');
-const path = require('path');
+import express from 'express';
+import paths from 'path';
+import posts from './routes/posts.js';
+const port = process.env.PORT || 8000; 
+
 const app = express();
 
-
-const port = process.env.PORT || 8000; 
-//setup static folder to serve static files
-// app.use(express.static(path.join(__dirname, 'public')));
-
-//routes    
-
-let posts = [
-    { id: 1, title: 'Best Post One'},
-    { id: 2, title: 'Silver Post Two'},
-    { id: 3, title: 'Post Three'},
-];
-
-// get all posts
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit) || posts.length;
-    if(!isNaN(limit) && limit > 0){ {
-    res.status(200).json(posts.slice(0, limit));
-    } } else {
-        res.status(400).json({ error: 'Invalid limit parameter' });
-    }
-});
-
-//get single post
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find(post => post.id === id);
-    // res.status(200).json(posts.filter(post => post.id === id));
-    if(!post){
-        res.status(404).json({ msg: `A Post with ID ${id} is not found :( Ja ja ja...!`});
-    }
-    else {
-        res.status(200).json(post);
-    }
-}); 
+//routes
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
