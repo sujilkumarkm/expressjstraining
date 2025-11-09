6be3ba7a6ebb8265349505f6ee0738c35e971465
sujilkumarkm <kmsujilkumar@gmail.com>
2025-11-09 21:39:54 +0000

    basic skelton

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//setup static folder to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes    
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.listen(8000, () => console.log('Server is running on port 8000'));
