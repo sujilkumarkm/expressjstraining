710c2a7267b687c78b3a62b6a7874270d5785751
sujilkumarkm <kmsujilkumar@gmail.com>
2025-11-10 01:06:17 +0000

    update post to array without db added

--- routes/posts.js
+++ routes/posts.js
@@ -46,4 +46,20 @@ router.post('/', (req, res) => {
     res.status(201).json(posts);
 });
 
+
+// update posts
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        return res
+        .status(404)
+        .json({ msg: `A Post with ID ${id} is not found :( OMG unlucky...!`});
+     }
+     post.title = req.body.title ? req.body.title : post.title;
+     res.status(200).json(posts);
+});
+
+
 export default router;
