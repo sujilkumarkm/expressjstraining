c432c2412eaab1e4bea34dbf504149f4df8d59b4
sujilkumarkm <kmsujilkumar@gmail.com>
2025-11-10 00:38:45 +0000

    code minor cleaning - making more readable

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
 import express from 'express';
 const router = express.Router();
 
-//routes    
 let posts = [
     { id: 1, title: 'Best Post One'},
     { id: 2, title: 'Silver Post Two'},
@@ -11,9 +10,10 @@ let posts = [
 // get all posts
 router.get('/', (req, res) => {
     const limit = parseInt(req.query.limit);
-    if(!isNaN(limit) && limit > 0){ {
+   
+    if(!isNaN(limit) && limit > 0) {
         return res.status(200).json(posts.slice(0, limit));
-    } } 
+    } 
         res.status(200).json(posts);
 });
 
@@ -29,5 +29,14 @@ router.get('/:id', (req, res) => {
     }
        res.status(200).json(post);
 }); 
+
+
+
+
+//create new post
+router.post('/', (req, res) => {
+        console.log(req.body);
+        res.status(201).json({ msg: 'Post created...'});
+    });
 
 export default router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import paths from 'path';
+import path from 'path';
 import posts from './routes/posts.js';
 const port = process.env.PORT || 8000; 
 
 const app = express();
 
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //routes
 app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
