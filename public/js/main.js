72243e68a2efa5ebc630a1992deeaedbc17f1fa7
sujilkumarkm <kmsujilkumar@gmail.com>
2025-11-10 03:27:19 +0000

    adding output to a frontend using vanila javascript

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+const output = document.querySelector('#output');
+const button = document.querySelector('#get-posts-btn');
+
+
+async function showPosts(){
+    try {
+        const res = await fetch('http://localhost:8080/api/posts');
+    if(!res.ok){
+        throw new Error('Failed to fetch posts');
+    }
+    const posts = await res.json();
+    output.innerHTML = '';
+
+
+    posts.forEach((post) => {
+        const postEl = document.createElement('div');
+        postEl.textContent = post.title;
+        output.appendChild(postEl);
+    });
+    } catch(error){
+        console.log('Error fetching posts');
+    
+    
+}
+}  
+
+// Event Listners
+button.addEventListener('click', showPosts);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';   
 import errorHandler from './middleware/error.js';
@@ -8,10 +9,17 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
 
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// setup static middleware
+app.use(express.static(path.join(path.join(__dirname, 'public'))));
 
 
 // logger middleware
